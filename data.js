cd98584a8134e676a6a1d5e31e8d6b894f2d6d2c
awen77 <148841221+awen77@users.noreply.github.com>
2025-06-28 17:01:43 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,17 @@
+
+// 软件数据：每类是一个数组，元素包含 name（软件名）、desc（描述）、link（蓝奏云链接）、code（提取码）
 const softwareData = {
   movie: [
-    {
-      name: "电影猎手 3.1.1.0",
-      desc: "",
-      link: "https://wwp.lanzoup.com/iOU872zcwx8d",
-      code: "3vjl",
-      date: "2025-06-28",
-      stars: 5
-    }
+    { name: "PotPlayer 高级版", desc: "本地播放器，支持外挂字幕", link: "#", code: "1234" },
+    { name: "MX Player 解锁版", desc: "安卓平台视频神器", link: "#", code: "abcd" }
   ],
-  novel: [],
-  gentle: [],
-  proxy: []
+  novel: [
+    { name: "猫耳FM", desc: "听书神器，支持下载", link: "#", code: "2222" }
+  ],
+  gentle: [
+    { name: "轻阅", desc: "纯净模式+私密空间", link: "#", code: "7890" }
+  ],
+  proxy: [
+    { name: "电影猎手", desc: "聚合版影视平台资源全速度快", link: "https://wwp.lanzoup.com/iOU872zcwx8d", code: "3vjl" }
+  ]
 };
