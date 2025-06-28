32169d8fc95fb4dc62661ae1265de7dece8e6928
awen77 <148841221+awen77@users.noreply.github.com>
2025-06-28 17:00:13 +0800

    Add files via upload
    init

---
+++ data.js
@@ -0,0 +1,15 @@
+const softwareData = {
+  movie: [
+    {
+      name: "电影猎手 3.1.1.0",
+      desc: "",
+      link: "https://wwp.lanzoup.com/iOU872zcwx8d",
+      code: "3vjl",
+      date: "2025-06-28",
+      stars: 5
+    }
+  ],
+  novel: [],
+  gentle: [],
+  proxy: []
+};
